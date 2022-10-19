a90c73da6b441b68dac0168c27095c3b1f500ba3
evripant <evripidispantelaios@gmail.com>
2022-10-19 21:01:18 +0300

    refactoring: improve responsiveness for header and cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -38,7 +38,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes.content}>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
