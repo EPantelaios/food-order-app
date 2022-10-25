7cc87359d3d2fe3b1064a0750d26abcdb4c5af5f
evripant <evripidispantelaios@gmail.com>
2022-10-25 18:58:24 +0300

    feature: add the authentication form to login, create new account

---
+++ src/components/Layout/Login.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import AuthContext from '../../store/auth-context';
+import classes from './Login.module.css';
+
+const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
+  return (
+    <div className={classes.headerButtons}>
+      {!isLoggedIn && (
+        <button>
+          <Link to="/login">Login</Link>
+        </button>
+      )}
+      {/* {isLoggedIn && (
+        <button>
+          <Link to="/profile">Profile</Link>
+        </button>
+      )} */}
+      {isLoggedIn && (
+        <button className={classes.buttonLogout} onClick={logoutHandler}>
+          <Link to="/login">Logout</Link>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default MainNavigation;
