9e26dbac64a4593aae4695e9642a45e4271d6d1d
evripant <evripidispantelaios@gmail.com>
2022-10-26 12:36:37 +0300

    feature: add NotFound page when navigate in non-existence URL

---
+++ src/components/Auth/NotFound.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import classes from './NotFound.module.css';
+
+const NotFound = () => {
+  return (
+    <div className={classes.returnPage}>
+      <h1>No match for this page...</h1>
+      <Link to="/">
+        <button type="button">Return to homepage</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,7 @@
+import NotFound from '../components/Auth/NotFound';
+
+const NotFoundPage = () => {
+  return <NotFound />;
+};
+
+export default NotFoundPage;
