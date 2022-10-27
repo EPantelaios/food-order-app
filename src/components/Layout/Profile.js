f2df3710fc5e42d633cb2630ec96bdf9abe70be7
evripant <evripidispantelaios@gmail.com>
2022-10-27 13:53:33 +0300

    feature: add dropdown menu at profile icon

--- src/components/Layout/Profile.js
+++ src/components/Layout/Profile.js
@@ -2,18 +2,13 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import AuthContext from '../../store/auth/auth-context';
+import ProfileDropdownMenu from './ProfileDropdownMenu';
 
-import { GrUserSettings } from 'react-icons/gr';
 import classes from './Profile.module.css';
 
 const Profile = () => {
   const authCtx = useContext(AuthContext);
-
   const isLoggedIn = authCtx.isLoggedIn;
-
-  const logoutHandler = () => {
-    authCtx.logout();
-  };
 
   return (
     <div className={classes.headerButtons}>
@@ -22,25 +17,10 @@ const Profile = () => {
           <button type="button">Login</button>
         </Link>
       )}
-
-      {/* <UserProfile /> */}
-
       {isLoggedIn && (
-        <span className={classes.profile}>
-          <Link to="/changepassword">
-            <button type="button">
-              <GrUserSettings size={32} />
-            </button>
-          </Link>
-        </span>
-      )}
-
-      {isLoggedIn && (
-        <Link to="/login">
-          <button className={classes.buttonLogout} onClick={logoutHandler}>
-            Logout
-          </button>
-        </Link>
+        <div className={classes.profile}>
+          <ProfileDropdownMenu />
+        </div>
       )}
     </div>
   );
---
+++ src/components/Layout/ProfileDropdownMenu.js
@@ -0,0 +1,43 @@
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import AuthContext from '../../store/auth/auth-context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { GrUserSettings } from 'react-icons/gr';
+
+import classes from './ProfileDropdownMenu.module.css';
+
+const ProfileDropdownMenu = () => {
+  const [dropdownShouldBeVisible, setDropdownShouldBeVisible] = useState(true);
+  const authCtx = useContext(AuthContext);
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
+  return (
+    <div className={classes.dropdown}>
+      <button type="button" className={classes.dropbtn}>
+        <GrUserSettings size={32} />
+        <FontAwesomeIcon icon={solid('caret-down')} color="black" />
+      </button>
+      <div
+        className={classes.dropdownContent}
+        style={{ display: !dropdownShouldBeVisible && 'none' }}
+        onClick={() => setDropdownShouldBeVisible(false)}
+        onMouseLeave={() => setDropdownShouldBeVisible(true)}
+      >
+        <Link to="profile">My Profile</Link>
+        <Link to="changepassword">Change Password</Link>
+        <Link to="login">
+          <span type="button" onClick={logoutHandler}>
+            Logout
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileDropdownMenu;
